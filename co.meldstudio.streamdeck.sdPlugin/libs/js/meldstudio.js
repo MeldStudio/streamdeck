3731d6aa5d061ac8bfbfe412e8abfdbb291f3496
Terry M <zv1n.fire@gmail.com>
2023-04-28 17:35:51 -0500

    Finalize release to Elgato store. (#1)

---
+++ co.meldstudio.streamdeck.sdPlugin/libs/js/meldstudio.js
@@ -0,0 +1,104 @@
+const MeldStudioEventEmitter = ELGEvents.eventEmitter();
+
+class MeldStudio {
+	on = MeldStudioEventEmitter.on;
+	emit = MeldStudioEventEmitter.emit;
+
+	connected = false;
+	ready = false;
+
+	constructor() {
+		this.connected = false;
+		this.initConnection();
+	}
+
+	initConnection() {
+		const address = '127.0.0.1';
+		const port = 13376;
+
+		this.socket = new WebSocket(`ws://${address}:${port}`);
+
+		this.socket.onopen = () => {
+			console.log('MeldStudio Connected!');
+
+			this.connected = true;
+			this.emit('connected');
+
+			this.channel = new QWebChannel(this.socket, (channel) => {
+				this.meld = channel.objects.meld;
+
+				this.meld.sessionChanged.connect(() => {
+					console.log('Session Changed');
+					this.emit('sessionChanged', this.meld.session);
+				});
+
+				this.meld.isStreamingChanged.connect(() => {
+					console.log('Streaming Changed');
+					this.emit('isStreamingChanged', this.meld.isStreaming);
+				});
+
+				this.meld.isRecordingChanged.connect(() => {
+					console.log('Recording Changed');
+					this.emit('isRecordingChanged', this.meld.isRecording);
+				});
+
+				this.ready = true;
+				this.emit('ready');
+				this.emit('sessionChanged', this.meld.session);
+				this.emit('isStreamingChanged', this.meld.isStreaming);
+				this.emit('isRecordingChanged', this.meld.isRecording);
+			});
+		};
+
+		this.socket.onclose = () => {
+			this.ready = false;
+			this.connected = false;
+			this.meld = {};
+
+			this.emit('closed');
+
+			setTimeout(() => {
+				this.initConnection();
+			}, 1000);
+		};
+	}
+}
+
+const $MS = new MeldStudio();
+
+class MeldStudioPlugin {
+	action;
+	contexts = {};
+
+	constructor(UUID) {
+		this.action = new Action(UUID);
+
+		this.action.onWillAppear(({ context }) => {
+			this.contexts[context] = { settings: {} };
+			$SD.getSettings(context);
+		});
+
+		this.action.onDidReceiveSettings(({ context, payload }) => {
+			this.setSettings(context, payload?.settings ?? {});
+		});
+	}
+
+	setSettings(context, settings) {
+		this.contexts[context].settings = settings;
+	}
+
+	getSettings(context) {
+		return this.contexts[context].settings;
+	}
+
+	updateState(context, state) {
+		$SD.setImage(context, state.image, 0);
+		$SD.setTitle(context, state.title);
+	}
+
+	forAllContexts(fn) {
+		for (const context in this.contexts) {
+			fn(context, this.getSettings(context));
+		}
+	}
+};
--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,4 @@ if [[ "$1" == "--fresh" ]]; then
     rm -rf ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/co.meldstudio.streamdeck.sdPlugin
 fi
 
-rsync -avz . ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/co.meldstudio.streamdeck.sdPlugin
+rsync -avz co.meldstudio.streamdeck.sdPlugin ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/
