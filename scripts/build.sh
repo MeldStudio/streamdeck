e80b901143322564a8a576c2a2c58c6f9759b7e3
Terry M <zv1n.fire@gmail.com>
2024-04-02 00:57:45 -0500

    Add upload to build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,12 @@ VERSION="$(grep '"Version": "' <co.meldstudio.streamdeck.sdPlugin/manifest.json 
 
 DistributionTool -b -i co.meldstudio.streamdeck.sdPlugin -o ./
 
-echo "Creating versioned plugin file: co.meldstudio.streamdeck.$VERSION.streamDeckPlugin"
-mv co.meldstudio.streamdeck.streamDeckPlugin co.meldstudio.streamdeck."$VERSION".streamDeckPlugin
+VERSIONED_NAME=co.meldstudio.streamdeck."$VERSION".streamDeckPlugin
+BUCKET_NAME=meld-software-updates
+
+echo "Creating versioned plugin file: ${VERSIONED_NAME}"
+mv co.meldstudio.streamdeck.streamDeckPlugin "${VERSIONED_NAME}"
+
+read -rp "CTRL-C if you do not want to upload to S3. Press any key to continue..."
+
+aws s3 cp "${VERSIONED_NAME}" s3://"${BUCKET_NAME}"/"${VERSIONED_NAME}"
