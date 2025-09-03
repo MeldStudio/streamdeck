8f316b21a9e9e2b859b806e432ef9bbb036ff911
Terry M <zv1n.fire@gmail.com>
2025-09-03 18:36:47 -0500

    Minor inspector fixups.

--- co.meldstudio.streamdeck.sdPlugin/libs/js/inspector.js
+++ co.meldstudio.streamdeck.sdPlugin/libs/js/inspector.js
@@ -2,164 +2,191 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
 class MeldStudioPropertyInspector {
-  settings = {};
+settings = {}
 
-  createOption(opt) {
-    const item = document.createElement("option");
 
-    item.innerHTML = opt.text;
-    item.value = opt.value;
-    item.selected = opt.selected;
 
-    return item;
-  }
 
-  // Get all items of a specific type.
-  getItemsByType(type, currentValue, parent, keep_filter) {
-    const items = $MS.meld?.session?.items ?? {};
+	createOption (opt) {
+		const item = document.createElement('option')
 
-    let selectedItems = [
-      {
-        text: "[No Selection]",
-        value: "",
-        selected: currentValue == "",
-      },
-    ];
+item.innerHTML = opt.text
+item.value = opt.value
+item.selected = opt.selected
 
-    for (let key in items) {
-      const item = items[key];
 
-	  if (keep_filter && !keep_filter(type, item)) continue;
+		return item
+	}
 
-      if (item.type != type) continue;
-      if (parent && item.parent != parent) continue;
+// Get all items of a specific type.
 
-      selectedItems.push({
-        text: item.name,
-        value: key,
-        selected: currentValue == key,
-      });
-    }
 
-    return selectedItems;
-  }
 
-  getValue(key) {
-    return this.settings[key] ?? "";
-  }
 
-  // Iterators over the items in the session and filters down
-  // each pass to the current type, filtered by the previous value.
-  updateSelection(elements, keep_filter) {
-    if (!$MS.meld) {
-      $MS.on("ready", () => {
-        this.updateSelection(elements);
-      });
-      $MS.on("sessionChanged", () => {
-        this.updateSelection(elements);
-      });
-      return;
-    }
+	getItemsByType (type, currentValue, parent, keep_filter) {
+		const items = $MS.meld?.session?.items ?? {}
 
-    let last = "";
-    for (let elid of elements) {
-      const value = this.getValue(elid);
-      const element = document.getElementById(elid);
+		let selectedItems = [
+			{
+				text: '[No Selection]',
+				value: '',
+				selected: currentValue == ''
+			}
+		]
 
-      let available = this.getItemsByType(elid, value, last,  keep_filter);
+		for (let key in items) {
+const item = items[key]
 
-      element.innerHTML = "";
-      element.disabled = false;
 
-      for (let opt of available) {
-        element.appendChild(this.createOption(opt));
-      }
 
-      last = value;
-    }
-  }
 
-  initializeSelection(
-    action,
-    elements,
-    data_provided = true,
-    keep_filter = null
-  ) {
-    for (let id of elements) {
-      const el = document.getElementById(id);
-      console.assert(el.id, "Select element not found");
-      if (!el) continue;
+			if (keep_filter && !keep_filter(type, item)) continue
 
-      if (this.settings[el.id] === undefined) this.settings[el.id] = "";
+			if (item.type != type) continue
+			if (parent && item.parent != parent) continue
 
-      el.onchange = () => {
-        if (!this.settings) return;
-        this.settings = { ...this.settings, [id]: el.value };
-        $PI.setSettings(this.settings);
-        if (data_provided) this.updateSelection(elements, keep_filter);
-      };
-    }
+			selectedItems.push({
+				text: item.name,
+				value: key,
+				selected: currentValue == key
+			})
+		}
 
-    $PI.onDidReceiveSettings(action, ({ action, payload }) => {
-      const { settings } = payload;
-      this.settings = settings;
+		return selectedItems
+	}
 
-      for (let field of elements) {
-        const dom_field = document.getElementById(field);
+	getValue (key) {
+		return this.settings[key] ?? ''
+	}
 
-        if (settings[field] === undefined) continue;
+// Iterators over the items in the session and filters down
+// each pass to the current type, filtered by the previous value.
 
-        dom_field.value = settings[field];
-      }
 
-      if (data_provided) this.updateSelection(elements, keep_filter);
-    });
 
-    $PI.getSettings();
-  }
 
-  initializeText(action, elements) {
-    for (let id of elements) {
-      const el = document.getElementById(id);
-      console.assert(el.id, "Input element not found");
-      if (!el) continue;
+	updateSelection (elements, keep_filter) {
+		if (!$MS.meld) {
+			$MS.on('ready', () => {
+				this.updateSelection(elements)
+			})
+			$MS.on('sessionChanged', session => {
+				console.log(elements, session)
+				this.updateSelection(elements)
+			})
+			return
+		}
+console.log(elements)
 
-      this.settings[el.id] = "";
 
-      el.onchange = () => {
-        if (!this.settings) return;
-        this.settings = { ...this.settings, [id]: el.value };
-        $PI.setSettings(this.settings);
-      };
-    }
 
-    $PI.onDidReceiveSettings(action, ({ action, payload }) => {
-      const { settings } = payload;
-      this.settings = settings;
 
-      for (let field of elements) {
-        const dom_field = document.getElementById(field);
-        if (settings[field] !== undefined) {
-          dom_field.value = settings[field];
-        } else {
-          // Use the default value specified by the field.
-          this.settings[field] = dom_field.value;
-        }
-      }
-    });
-  }
+		let last = ''
+		for (let elid of elements) {
+const value = this.getValue(elid)
+const element = document.getElementById(elid)
 
-  watchConnections(id) {
-    $MS.on("connected", () => {
-      document.getElementById(id).style = "display: none;";
-    });
 
-    $MS.on("closed", () => {
-      document.getElementById(id).style = "";
-    });
 
-    if ($MS.connected) document.getElementById(id).style = "display: none;";
-  }
+console.log(elid)
+
+let available = this.getItemsByType(elid, value, last, keep_filter)
+
+element.innerHTML = ''
+element.disabled = false
+
+
+			for (let opt of available) {
+				element.appendChild(this.createOption(opt))
+			}
+
+			last = value
+		}
+	}
+
+	initializeSelection (action, elements, data_provided = true, keep_filter = null) {
+		for (let id of elements) {
+			const el = document.getElementById(id)
+			console.assert(el.id, 'Select element not found')
+			if (!el) continue
+
+if (this.settings[el.id] === undefined) this.settings[el.id] = ''
+
+
+			el.onchange = () => {
+				if (!this.settings) return
+				this.settings = { ...this.settings, [id]: el.value }
+				$PI.setSettings(this.settings)
+				if (data_provided) this.updateSelection(elements, keep_filter)
+			}
+		}
+
+		$PI.onDidReceiveSettings(action, ({ action, payload }) => {
+const { settings } = payload
+this.settings = settings
+
+
+			for (let field of elements) {
+const dom_field = document.getElementById(field)
+
+
+				if (settings[field] === undefined) continue
+
+				dom_field.value = settings[field]
+			}
+
+			if (data_provided) this.updateSelection(elements, keep_filter)
+		})
+
+		$PI.getSettings()
+	}
+
+	initializeText (action, elements) {
+		for (let id of elements) {
+			const el = document.getElementById(id)
+			console.assert(el.id, 'Input element not found')
+			if (!el) continue
+
+this.settings[el.id] = ''
+
+
+			el.onchange = () => {
+				if (!this.settings) return
+				this.settings = { ...this.settings, [id]: el.value }
+				$PI.setSettings(this.settings)
+			}
+		}
+
+		$PI.onDidReceiveSettings(action, ({ action, payload }) => {
+const { settings } = payload
+this.settings = settings
+
+
+			for (let field of elements) {
+				const dom_field = document.getElementById(field)
+				if (settings[field] !== undefined) {
+					dom_field.value = settings[field]
+				} else {
+					// Use the default value specified by the field.
+					this.settings[field] = dom_field.value
+				}
+			}
+		})
+	}
+
+	watchConnections (id) {
+		$MS.on('connected', () => {
+			document.getElementById(id).style = 'display: none;'
+		})
+
+$MS.on('closed', () => {
+	document.getElementById(id).style = ''
+})
+
+
+		if ($MS.connected) document.getElementById(id).style = 'display: none;'
+	}
 }
 
-const $MSPI = new MeldStudioPropertyInspector();
+const $MSPI = new MeldStudioPropertyInspector()
+
